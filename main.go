9237706b985f20aed48b272d0f7974a099fec506
Sergey Gorokhov <sergey.login@gmail.com>
2022-09-29 23:37:36 +0300

    add function "token" to get token

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package read_adviser_bot
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	t := mustToken()
+
 	// tgClient = telegram.New(token)
 
 	// fetcher = fetcher.New()
@@ -9,4 +16,20 @@ func main() {
 
 	// consumer.Start(fetcher, processor)
 
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token os not specified")
+	}
+
+	return *token
 }
