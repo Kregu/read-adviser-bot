52e288dbb7c2a7a97b195869643e115d3c3d9990
Sergey Gorokhov <sergey.login@gmail.com>
2022-09-29 23:27:03 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package read_adviser_bot
+
+func main() {
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New()
+
+	// processor = processor.New()
+
+	// consumer.Start(fetcher, processor)
+
+}
