c812604fd24b74c9c1ee8877da9896a5ad837f4e
Sergey Gorokhov <sergey.login@gmail.com>
2023-12-05 19:08:49 +0300

    fix problem: dot't handle events

--- consumer/event_consumer/event_consumer.go
+++ consumer/event_consumer/event_consumer.go
@@ -2,19 +2,20 @@ package event_consumer
 
 import (
 	"log"
-	"read-adviser-bot/events"
 	"time"
+
+	"read-adviser-bot/events"
 )
 
 type Consumer struct {
-	fetcher events.Fetcher
+	fetcher   events.Fetcher
 	processor events.Processor
 	batchSize int
 }
 
 func New(fetcher events.Fetcher, processor events.Processor, batchSize int) Consumer {
 	return Consumer{
-		fetcher: fetcher,
+		fetcher:   fetcher,
 		processor: processor,
 		batchSize: batchSize,
 	}
@@ -25,7 +26,7 @@ func (c Consumer) Start() error {
 		gotEvents, err := c.fetcher.Fetch(c.batchSize)
 		if err != nil {
 			log.Printf("[ERR] consumer: %s", err.Error())
-			
+
 			continue
 		}
 
@@ -34,14 +35,20 @@ func (c Consumer) Start() error {
 
 			continue
 		}
+
+		if err := c.handleEvents(gotEvents); err != nil {
+			log.Print(err)
+
+			continue
+		}
 	}
 }
 
 func (c Consumer) handleEvents(events []events.Event) error {
-	for _, event:=range events {
+	for _, event := range events {
 		log.Printf("got new event: %s", event.Text)
 
-		if err := c.processor.Process(event);err!=nil{
+		if err := c.processor.Process(event); err != nil {
 			log.Printf("can't handle event: %s", err.Error())
 
 			continue
